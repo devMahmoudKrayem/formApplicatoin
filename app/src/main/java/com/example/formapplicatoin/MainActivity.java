e69640d0f6a439b5aad3d03aa9881f642e800e9d
Mahmoud Krayem <114818946+devMahmoudKrayem@users.noreply.github.com>
2023-03-05 14:19:01 +0200

    update

--- app/src/main/java/com/example/formapplicatoin/MainActivity.java
+++ app/src/main/java/com/example/formapplicatoin/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,36 @@ public class MainActivity extends AppCompatActivity {
         RadioButton three=findViewById(R.id.radbt_3);
         TextView result=findViewById(R.id.tv_1);
         Button save=findViewById(R.id.bt_save);
+        CheckBox ch1=findViewById(R.id.chb_1);
+        CheckBox ch2=findViewById(R.id.chb_2);
+        CheckBox ch3=findViewById(R.id.chb_3);
+        CheckBox ch4=findViewById(R.id.chb_4);
+        ch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Java", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        ch2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "pyhton", Toast.LENGTH_SHORT).show();
+
+            }
+        }); ch3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "C++", Toast.LENGTH_SHORT).show();
+
+            }
+        }); ch4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Php", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
